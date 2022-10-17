35be8e3ed7b13d38cd516c42987255c950d1d2d2
john-vasile <john.Vs11@hotmail.com>
2022-10-16 22:09:40 -0400

    connect to cloud MongoDb

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config;
+// const express = require("express");
+// const router = express.Router();
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 5000;
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || `mongodb://localhost/localTravelDB`;
+
+console.log("connect to db", MONGODB_URI);
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+  })
+  .then((x) => console.log(`connected to MongoDB!: "${x.connections[0].name}"`))
+  .catch((err) =>
+    console.error("Error connectiong to mongoDB*********************", err)
+  );
