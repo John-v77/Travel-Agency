ddaba0e2ad67e29ccca3891bc4388620ba01aa19
john-vasile <john.Vs11@hotmail.com>
2022-12-13 16:08:00 -0500

    add Login Route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,14 @@
 const User = require("../models/usersModel");
 const jwt = require("jsonwebtoken");
+
+// # signToken
+const signToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+// #1 Sign Up
 const signup = async (req, res, next) => {
   try {
     const newUser = await User.create({
@@ -9,11 +18,10 @@ const signup = async (req, res, next) => {
       passwordConfirm: req.body.passwordConfirm,
     });
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
+    const token = signToken(newUser._id);
     res.status(201).json({
       status: "success",
+      token,
       data: {
         user: newUser,
       },
@@ -26,6 +34,43 @@ const signup = async (req, res, next) => {
   }
 };
 
+// #2 Login
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1 Check if email and password exist
+    if (!email || !password) {
+      res.status(400).json({
+        status: "fail",
+        message: "email or password missing!",
+      });
+      return next();
+    }
+    // 2 Check if user exists && password is correct
+    const user = await User.findOne({ email: email }).select("+password");
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      res.status(401).json({
+        status: "fail",
+        message: "incorect email or password",
+      });
+      return next();
+    }
+    // 3 if everything is ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+      status: "success",
+      token: token,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
 module.exports = {
   signup,
+  login,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const authControler = require("../controllers/authController");
 
 router.post("/signup", authControler.signup);
+router.post("/login", authControler.login);
 module.exports = router;
