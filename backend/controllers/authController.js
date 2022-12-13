67fdf58617361c7825fd4f060e1340797490f9c1
john-vasile <john.Vs11@hotmail.com>
2022-12-13 12:50:06 -0500

    fix security issue - assign admin role

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,12 @@
 const User = require("../models/usersModel");
 const signup = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
     res.status(201).json({
       status: "success",
       data: {
