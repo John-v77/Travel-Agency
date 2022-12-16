d0583b262d7160f6061ae19612581b7c0c1c611e
john-vasile <john.Vs11@hotmail.com>
2022-12-16 18:01:31 -0500

    add/set default fonts, font sizes, font colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-xl font-bold text-red-400 underline">Hello world!</h1>
+  );
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      lato: ["Lato", "Open Sans"],
+      roboto: ["Roboto", "Open Sans"],
+    },
+    fontSize: {
+      title: `2.6rem;`,
+      paragraph: `1.2rem;`,
+    },
+    extend: {
+      colors: {
+        primary: "#FFFF63",
+        secondary: "#000001",
+        decor: "#222222",
+      },
+    },
   },
   plugins: [],
 };
