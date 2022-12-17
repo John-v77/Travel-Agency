8473b0aaca30a249b394dec6b44c1aa7d833d925
John-v77 <john.vs11@hotmail.com>
2022-12-16 20:06:35 -0500

    adds menu for mobile version

--- client/src/components/Features/navBar/Navbar.jsx
+++ client/src/components/Features/navBar/Navbar.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import { BsPerson } from 'react-icons/bs';
+import { BiSearch } from 'react-icons/bi';
+import { AiOutlineClose } from 'react-icons/ai';
+import { HiOutlineMenuAlt4 } from 'react-icons/hi';
+
 function Navbar(props) {
+  const [mobNav, setMobNav] = useState(false);
+
+  const handleNav = () => {
+    setMobNav(!mobNav);
+  };
   return (
     <div className='bg-gray-100 w-full p-4 flex justify-between'>
       <h1 className=''>Travel.</h1>
@@ -17,8 +28,31 @@ function Navbar(props) {
       </ul>
 
       <div className='hidden md:flex'>
-        <p>search</p>
-        <p>login</p>
+        <BiSearch size={20} />
+        <BsPerson size={20} />
+      </div>
+
+      <div onClick={handleNav} className='md:hidden  z-10 mobile-menu-btn'>
+        {mobNav ? (
+          <HiOutlineMenuAlt4 size={20} />
+        ) : (
+          <AiOutlineClose size={20} />
+        )}
+      </div>
+
+      <div className='mobile-menu w-full absolute left-0 top-0 bg-gray-200 p-4 px-6 md:hidden'>
+        <h1 className=''>Travel.</h1>
+        <ul className='flex flex-col md:hidden '>
+          <li className='mt-2 py-4 border-b border-slate-100'>
+            <Link to='/'>Home</Link>
+          </li>
+          <li className='mt-2 py-4 border-b border-slate-100'>
+            <Link to='/page1'>Page1</Link>
+          </li>
+          <li className='mt-2 py-4 border-b border-slate-100'>
+            <Link to='/page2'>Page2</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
