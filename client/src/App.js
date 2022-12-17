c52398f5e2831117fe414310a912978b298ab1e4
John-v77 <john.vs11@hotmail.com>
2022-12-16 19:42:32 -0500

    adds react-router-dom & links for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Features/navBar/Navbar';
+import Home from './components/Pages/Home/Home';
 
 function App() {
   return (
     <div className='max-w-[1600px] mx-auto'>
       <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='page1' element={<Home />} />
+        <Route path='page2' element={<Home />} />
+        <Route path='page3' element={<Home />} />
+      </Routes>
       <p className='text-3xl font-bold  text-red-200'>Hello world!</p>
     </div>
   );
--- client/src/components/Features/navBar/Navbar.jsx
+++ client/src/components/Features/navBar/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function Navbar(props) {
   return (
     <div className='bg-gray-100 w-full p-4 flex justify-between'>
       <h1 className=''>Travel.</h1>
       <ul className='hidden md:flex'>
-        <li>Home</li>
-        <li>Page1</li>
-        <li>Page2</li>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/page1'>Page1</Link>
+        </li>
+        <li>
+          <Link to='/page2'>Page2</Link>
+        </li>
       </ul>
 
       <div className='hidden md:flex'>
