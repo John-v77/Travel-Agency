1ad79fc0bfeafa4c0559d07983c7bfda17fb6582
John-v77 <john.vs11@hotmail.com>
2022-12-16 19:08:53 -0500

    add custom colors and screensizes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,11 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976x',
+      xl: '1440px',
+      xxl: '1536px',
+    },
+
     fontFamily: {
       accent: ['Lato', 'Open Sans'],
       pragraph: ['Roboto', 'Open Sans'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        prim1: '#004e98',
+        prim2: '#3a6ea5',
+        back3: '#c0c0c0',
+        back4: '#ebebeb',
+        acce5: '#ff6700',
+      },
+    },
   },
   plugins: [],
 };
