16297484147f901bf8b2249ead777416800da992
john-vasile <john.Vs11@hotmail.com>
2022-12-17 16:58:48 -0500

    Home page - popular section

---
+++ client/src/assets/images.utils.js
@@ -0,0 +1,20 @@
+export const header_img =
+  "https://www.ncl.com/sites/default/files/715800_New_Year_Preview_mobile_webhero_1536x1020.jpg";
+
+export const rome =
+  "https://image.cnbcfm.com/api/v1/image/106899929-1624114232378-gettyimages-1054301494-237a9700aereo.jpeg?v=1624114258";
+
+export const white_dress =
+  "https://assets.vogue.in/photos/603780b297a6d928a4e6eae9/16:9/w_1280,c_limit/Travel.jpg";
+
+export const happy_couple =
+  "https://imageio.forbes.com/specials-images/imageserve/631b2f97a60db35ea6b586d2/fall-travel-deals/0x0.jpg?format=jpg&crop=1967,1446,x808,y0,safe&width=960";
+
+export const london =
+  "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2017_20/2000986/170515-better-travelling-healthy-london-se-521p.jpg";
+
+export const paris =
+  "https://a.storyblok.com/f/51678/1000x1118/c64598e652/itinerary-hero-mobile-1-parb.jpg";
+
+export const tailand =
+  "https://apicms.thestar.com.my/uploads/images/2022/11/04/1803987.jpg";
--- client/src/pages/Home/Home.page.jsx
+++ client/src/pages/Home/Home.page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { paris, tailand } from "../../assets/images.utils";
 
 function Home(props) {
   return (
@@ -16,6 +17,71 @@ function Home(props) {
           <p>Explore it with us</p>
         </div>
       </header>
+
+      {/* New Deals */}
+      <section className="NewDeals mt-20">
+        <h2 className="text-[20px]">Our new Deals</h2>
+
+        {/* Item 1 */}
+        <div className="DestinationContainer w-80 mx-auto rounded-t-sm mt-20">
+          <img src={paris} alt="paris" className="80w rounded-t-sm" />
+          <div className="flex justify-between bg-green-800 h-16 text-white items-center px-4 uppercase rounded-b-sm">
+            <p>Paris</p>
+            <p>$6000</p>
+          </div>
+        </div>
+
+        {/* Item 1 */}
+        <div className="DestinationContainer w-80 mx-auto rounded-t-sm mt-20">
+          <img src={paris} alt="paris" className="80w rounded-t-sm" />
+          <div className="flex justify-between bg-green-800 h-16 text-white items-center px-4 uppercase rounded-b-sm">
+            <p>Paris</p>
+            <p>$6000</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="MostPopular mt-20 pt-20 bg-[#E5E5E5]">
+        <h2 className="text-[20px]">Popular Deals</h2>
+
+        {/* Item 2 */}
+        <div className="DestinationContainer w-64 mx-auto rounded-t-sm mt-20">
+          <img
+            src="https://apicms.thestar.com.my/uploads/images/2022/11/04/1803987.jpg"
+            alt="tailand"
+            className="rounded-t-sm h-56"
+          />
+          <div className="bg-white h-32 text-left pt-6 pl-2">
+            <p className="text-[16px]">Tailand</p>
+            <br></br>
+            <p className="text-[16px] text-slate-400">March 2023</p>
+          </div>
+          <div className="flex justify-between bg-white border-t h-12 items-center px-4 rounded-b-sm">
+            <p className="text-[24px]">♡</p>
+            <p>Travel</p>
+            <p className="text-[20px]">☆</p>
+          </div>
+        </div>
+
+        {/* Item 2 */}
+        <div className="DestinationContainer w-64 mx-auto rounded-t-sm mt-20">
+          <img
+            src="https://apicms.thestar.com.my/uploads/images/2022/11/04/1803987.jpg"
+            alt="tailand"
+            className="rounded-t-sm h-56"
+          />
+          <div className="bg-white h-32 text-left pt-6 pl-2">
+            <p className="text-[16px]">Tailand</p>
+            <br></br>
+            <p className="text-[16px] text-slate-400">March 2023</p>
+          </div>
+          <div className="flex justify-between bg-white border-t h-12 items-center px-4 rounded-b-sm">
+            <p className="text-[24px]">♡</p>
+            <p>Travel</p>
+            <p className="text-[20px]">☆</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
