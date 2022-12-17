a2c7c25febd846dd93280c723dbb7ec44ea41786
John-v77 <john.vs11@hotmail.com>
2022-12-16 19:30:34 -0500

    adds maximum width for app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <p className='text-3xl font-bold  text-red-200'>Hello world!</p>;
+  return (
+    <div className='max-w-[1600px] mx-auto'>
+      <p className='text-3xl font-bold  text-red-200'>Hello world!</p>
+    </div>
+  );
 }
 
 export default App;
