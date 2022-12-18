e7bd2cc99799215f55d8c04e2b845eedb054466a
John-v77 <john.vs11@hotmail.com>
2022-12-17 22:02:24 -0500

    adds title to ameneties feature

--- client/src/components/Features/ameneties/Ameneties.jsx
+++ client/src/components/Features/ameneties/Ameneties.jsx
@@ -3,10 +3,16 @@ import { ameneties } from '../../../data/data';
 import AmenetiesCard from './AmenetiesCard';
 function Ameneties(props) {
   return (
-    <div className='max-w-[1600px] mx-auto  py-16 grid grid-cols-2 gap-3 lg:grid-cols-3 xl:gap4'>
-      {ameneties.map((item, index) => {
-        return <AmenetiesCard bg={item.image} text={item.name} />;
-      })}
+    <div className='max-w-[1600px] mx-auto  py-16 '>
+      <div className='p-4'>
+        <h1>Amazing ameneties</h1>
+        <p>have a great time</p>
+      </div>
+      <div className='grid grid-cols-2 gap-3 lg:grid-cols-3 xl:gap4'>
+        {ameneties.map((item, index) => {
+          return <AmenetiesCard bg={item.image} text={item.name} />;
+        })}
+      </div>
     </div>
   );
 }
