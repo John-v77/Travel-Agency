03168ccae05e4df6d14b993834ea0be198e4626e
John-v77 <john.vs11@hotmail.com>
2022-12-18 11:57:52 -0500

    add Featured Destinations

---
+++ client/src/components/Features/featuredDestinations/FeaturedDes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { goodPictures } from '../../../data/data';
+import FeaturedDesCard from './FeaturedDesCard';
+
+function FeaturedDes(props) {
+  return (
+    <div className='max-w-[1600px] mx-auto py-12 md:p-4 grid md:grid-cols-3 gap-6 md:mb-6'>
+      {goodPictures.map((item, index) => {
+        if (index < 3) {
+          return <FeaturedDesCard bg={item.image} />;
+        }
+      })}
+    </div>
+  );
+}
+
+export default FeaturedDes;
---
+++ client/src/components/Features/featuredDestinations/FeaturedDesCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function FeaturedDesCard(props) {
+  return (
+    <div className='rounded-xl relative'>
+      <div>
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white text-center'>
+          <p className='font-bold text-2xl pt-4'>Sun's Out</p>
+          <p className='px-2'>Throught 8/26</p>
+
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4 right-4 p-1.5'>
+            Book Now
+          </button>
+        </div>
+        <img
+          className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src={props.bg}
+          alt=''
+        />
+      </div>
+    </div>
+  );
+}
+
+export default FeaturedDesCard;
