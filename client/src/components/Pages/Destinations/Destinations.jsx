c730660354cb282e326988ff9ba1efc305b078d1
john-vasile <john.Vs11@hotmail.com>
2023-07-29 17:54:05 -0400

    fetch destinations

--- client/src/components/Pages/Destinations/Destinations.jsx
+++ client/src/components/Pages/Destinations/Destinations.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FeaturedDes from '../../Features/featuredDestinations/FeaturedDes';
 import { useDispatch, useSelector } from 'react-redux';
 import { increment } from '../../../store/counterSlice';
+import actions from '../../../api';
 function Destinations(props) {
   const { coin } = useSelector((state) => {
     return state.counter;
   });
 
+  const [destination, setDestinations] = useState([]);
+
   // console.log(counter, 'hello1');
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    actions.getDestinations().then((res) => {
+      console.log('do we have res', res);
+      setDestinations(res);
+    });
+  }, []);
+
+  console.log(destination, 'are destinations set?');
   return (
     <div className='max-w-[1600px] mx-auto md:p-0'>
       <div className='max-h-[500px] relative'>
@@ -24,6 +35,20 @@ function Destinations(props) {
           src='https://imageio.forbes.com/specials-images/dam/imageserve/1171238184/0x0.jpg?format=jpg&width=1600'
           alt='/'
         />
+      </div>
+
+      <div>
+        {destination.map((el, key) => {
+          return (
+            <div key={key}>
+              <h1>{el.name}</h1>
+
+              <h2>{el.price}</h2>
+
+              <p>{el.description}</p>
+            </div>
+          );
+        })}
       </div>
 
       <FeaturedDes />
