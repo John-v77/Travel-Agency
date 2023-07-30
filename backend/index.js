e39a75c0729f6095fcc6cf002774128b8588a388
john-vasile <john.Vs11@hotmail.com>
2023-07-29 23:18:07 -0400

    testing middleware

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const express = require('express');
-const vacantionRouter = require('./routes/vacantionRoutes');
-const userRouter = require('./routes/userRoutes');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const vacantionRouter = require("./routes/vacantionRoutes");
+const userRouter = require("./routes/userRoutes");
 const PORT = process.env.PORT || 5000;
 const app = express();
-const path = require('path');
-const cors = require('cors');
+const path = require("path");
+const cors = require("cors");
 
 // Body parser, reads data from body into req.body - need json requests
-app.use(express.json({ limit: '10kb' })); // for application/json
+app.use(express.json({ limit: "10kb" })); // for application/json
 // app.use(express.urlencoded());
 
 const MONGODB_URI =
@@ -22,13 +22,13 @@ mongoose
     serverSelectionTimeoutMS: 5000,
   })
   .then((x) => console.log(`connected to MongoDB!: "${x.connections[0].name}"`))
-  .catch((err) => console.error('Error connectiong to mongoDB', err));
+  .catch((err) => console.error("Error connectiong to mongoDB", err));
 
 //fix cors error
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000'], //Swap this with the client url
+    origin: ["http://localhost:3000"], //Swap this with the client url
   })
 );
 
@@ -40,15 +40,15 @@ app.use(
 // });
 
 // Testing middleware
-// app.use((req, res, next) => {
-//   req.requstTime = new Date().toISOString();
-//   console.log(req.headers);
-//   next();
-// });
+app.use((req, res, next) => {
+  req.requstTime = new Date().toISOString();
+  console.log("what are the headers", req.headers);
+  next();
+});
 
 // Routers
-app.use('/api/v1/vacantions', vacantionRouter);
-app.use('/api/v1/user', userRouter);
+app.use("/api/v1/vacantions", vacantionRouter);
+app.use("/api/v1/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
