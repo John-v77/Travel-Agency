224858e4252a30a9aa2cb5c20b2b4c7525ec69a6
john-vasile <john.Vs11@hotmail.com>
2023-07-30 17:07:27 -0400

    add Catch Async Function for controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const { promisify } = require("util");
 const User = require("../models/usersModel");
 const jwt = require("jsonwebtoken");
+const catchAsync = require("../utils/catchAsync");
 
 // # signToken
 const signToken = (id) => {
@@ -10,147 +11,119 @@ const signToken = (id) => {
 };
 
 // #1 Sign Up
-const signup = async (req, res, next) => {
-  try {
-    const { name, email, password, passwordConfirm } = req.body;
-    console.log("signup TT", email, password, passwordConfirm, name);
-    const newUser = await User.create({
-      name: name,
-      email: email,
-      password: password,
-      passwordConfirm: passwordConfirm,
-    });
+const signup = catchAsync(async (req, res, next) => {
+  const { name, email, password, passwordConfirm } = req.body;
+  console.log("signup TT", email, password, passwordConfirm, name);
+  const newUser = await User.create({
+    name: name,
+    email: email,
+    password: password,
+    passwordConfirm: passwordConfirm,
+  });
 
-    const token = signToken(newUser._id);
+  const token = signToken(newUser._id);
 
-    console.log(token, "what is the token", newUser);
-    res.status(201).json({
-      status: "success",
-      token,
-      data: {
-        user: newUser,
-      },
-    });
-  } catch (error) {
-    console.log(err, ":err!!!!!!!!!!!!!!!!!!!");
-    res.status(404).json({
-      status: "fail",
-      message: error,
-    });
-  }
-};
+  console.log(token, "what is the token", newUser);
+  res.status(201).json({
+    status: "success",
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
 
 // #2 Login
-const login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
 
-    // 1 Check if email and password exist
-    if (!email || !password) {
-      res.status(400).json({
-        status: "fail",
-        message: "email or password missing!",
-      });
-      return next();
-    }
-    // 2 Check if user exists && password is correct
-    const user = await User.findOne({ email: email }).select("+password");
-    if (!user || !(await user.correctPassword(password, user.password))) {
-      res.status(401).json({
-        status: "fail",
-        message: "incorect email or password",
-      });
-      return next();
-    }
-    // 3 if everything is ok, send token to client
-    const token = signToken(user._id);
-    console.log("is token working", token, "\n");
-
-    res.status(200).json({
-      status: "success",
-      token: token,
-      userName: user.name,
-    });
-  } catch (err) {
-    res.status(404).json({
+  // 1 Check if email and password exist
+  if (!email || !password) {
+    res.status(400).json({
       status: "fail",
-      message: err,
+      message: "email or password missing!",
     });
+    return next();
   }
-};
+  // 2 Check if user exists && password is correct
+  const user = await User.findOne({ email: email }).select("+password");
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    res.status(401).json({
+      status: "fail",
+      message: "incorect email or password",
+    });
+    return next();
+  }
+  // 3 if everything is ok, send token to client
+  const token = signToken(user._id);
+  console.log("is token working", token, "\n");
+
+  res.status(200).json({
+    status: "success",
+    token: token,
+    userName: user.name,
+  });
+});
 
 // Get all
-const getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
+const getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
 
-    console.log("users!!!111", users);
+  console.log("users!!!111", users);
 
-    res.status(200).json({
-      status: "success",
-      results: users.length,
-      data: { users },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: { users },
+  });
+});
 
 // #3 Protect Routes
 
-const protect = async (req, res, next) => {
+const protect = catchAsync(async (req, res, next) => {
   let token;
-  try {
-    // get token check if exists
-    const { authorization } = req.headers;
-    if (authorization && authorization.startsWith("Bearer")) {
-      token = authorization.split(" ")[1];
-    }
-    if (!token) {
-      res.status(401).json({
-        status: "fail",
-        message: "Unauthorized",
-      });
-      return next();
-    }
 
-    // validate token
-    const decodedPayload = await promisify(jwt.verify)(
-      token,
-      process.env.JWT_SECRET
-    );
-
-    // check user still exists
-    const freshUser = await User.findById(decodedPayload.id);
-
-    if (!freshUser) {
-      res.status(401).json({
-        status: "fail",
-        message: "The user no longer exists",
-      });
-      return next();
-    }
-    // check if user changed pass after token was issued
-    if (freshUser.changedPasswordAfter(decodedPayload.iat)) {
-      res.status(401).json({
-        status: "fail",
-        message: "Password change recently, login again",
-      });
-      next();
-    }
-    //  grant access to protected route
-    req.user = freshUser;
-    return next();
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
+  // get token check if exists
+  const { authorization } = req.headers;
+  if (authorization && authorization.startsWith("Bearer")) {
+    token = authorization.split(" ")[1];
   }
-};
+  if (!token) {
+    res.status(401).json({
+      status: "fail",
+      message: "Unauthorized",
+    });
+    return next();
+  }
+
+  // validate token
+  const decodedPayload = await promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET
+  );
+
+  // check user still exists
+  const freshUser = await User.findById(decodedPayload.id);
+
+  if (!freshUser) {
+    res.status(401).json({
+      status: "fail",
+      message: "The user no longer exists",
+    });
+    return next();
+  }
+  // check if user changed pass after token was issued
+  if (freshUser.changedPasswordAfter(decodedPayload.iat)) {
+    res.status(401).json({
+      status: "fail",
+      message: "Password change recently, login again",
+    });
+    next();
+  }
+  //  grant access to protected route
+  req.user = freshUser;
+  return next();
+});
 
 module.exports = {
   signup,
--- backend/controllers/vacantionController.js
+++ backend/controllers/vacantionController.js
@@ -1,94 +1,60 @@
 const Destination = require("../models/destinationModel");
+const catchAsync = require("../utils/catchAsync");
 
 // Get all
-const getAllVacantionPackages = async (req, res) => {
-  try {
-    const destinations = await Destination.find();
+const getAllVacantionPackages = catchAsync(async (req, res, next) => {
+  const destinations = await Destination.find();
 
-    res.status(200).json({
-      status: "success",
-      results: destinations.length,
-      data: { destinations },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    results: destinations.length,
+    data: { destinations },
+  });
+});
 
 // Get one by id
-const getVacantionPackage = async (req, res) => {
-  try {
-    const destination = await Destination.findById(req.params.id);
-    res.status(200).json({
-      status: "success",
-      data: { destination },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+const getVacantionPackage = catchAsync(async (req, res) => {
+  const destination = await Destination.findById(req.params.id);
+  res.status(200).json({
+    status: "success",
+    data: { destination },
+  });
+});
 
 // Create vacation package
-const createVacantionPackage = async (req, res) => {
-  try {
-    const newDestination = await Destination.create(req.body);
-    res.status(201).json({
-      status: "success",
-      data: { destination: newDestination },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+const createVacantionPackage = catchAsync(async (req, res, next) => {
+  const newDestination = await Destination.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: { destination: newDestination },
+  });
+});
 
 // Create vacation package
-const updateVacationPackage = async (req, res) => {
-  try {
-    const destinationPackage = await Destination.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
+const updateVacationPackage = catchAsync(async (req, res) => {
+  const destinationPackage = await Destination.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
 
-    res.status(200).json({
-      status: "success",
-      data: { destination: destinationPackage },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: { destination: destinationPackage },
+  });
+});
 
 // Delete vacation package
-const deleteVacantionPackage = async (req, res) => {
-  try {
-    await Destination.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: "success",
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+const deleteVacantionPackage = catchAsync(async (req, res) => {
+  await Destination.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
 
 module.exports = {
   getAllVacantionPackages,
