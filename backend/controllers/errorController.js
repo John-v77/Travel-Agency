87a19bb8e619f70007b3b157aeee5d26b04b7356
john-vasile <john.Vs11@hotmail.com>
2023-08-02 08:10:55 -0400

    error Controller > dev vs production

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -1,9 +1,46 @@
+const AppError = require("../utils/appError");
+const colors = require("colors");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    console.log("yellow".yellow);
+    let error = { ...err };
+    if (error.name === "CastError") {
+      handleCastErrorDB(error);
+    }
+    sendErrorProd(error, res);
+  }
 };
