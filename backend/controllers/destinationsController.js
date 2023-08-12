03ab314372481ed19df4b25fbf5c1a91e3d5385f
john-vasile <john.Vs11@hotmail.com>
2023-08-12 13:38:59 -0400

    add APIFeature class

--- backend/controllers/destinationsController.js
+++ backend/controllers/destinationsController.js
@@ -2,59 +2,24 @@ const Destination = require("../models/destinationModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const colors = require("colors");
+const APIFeatures = require("../utils/apiFeatures");
 
 // Get all
-
 // Fetch all destinations
 // route GET /api/vacantions || api/v1/vacantions?price[gte]=700
 // access Public
 const getAllDestinationPackages = catchAsync(async (req, res, next) => {
-  // 1 # filtering
-  // get query obj
-  const queryObj = { ...req.query };
-  const excludedFields = ["page", "sort", "limit", "fields"];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
-  // adjust query for <less then> <more then> lte, gte, gt, lt
-  let queryStr = JSON.stringify(queryObj);
-
-  // uses regex to patern match and add '$'
-  queryStr = queryStr.replace(/\b(gte|lte|gt|lt)\b/g, (match) => `$${match}`);
-
-  let query = Destination.find(JSON.parse(queryStr));
-
-  // 2 # sorting res
-  if (req.query.sort) query = query.sort();
-
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("createdAt");
-  }
-
-  // 3 # custom fiels query
-  if (req.query.fields) {
-    const fields = req.query.fields.split(",").join(" ");
-    query = query.select(fields);
-  } else {
-    query.select("-__id");
-  }
-
-  // 4 # paginations
-
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 20;
-  const skip = (page - 1) * limit;
-  query = query.skip(10).limit(10);
-
-  if (req.query.page) {
-    const numDestinations = await Destination.countDocuments();
-    if (skip >= numDestinations) throw new Error("This page does not exists");
-  }
-
+  // let query = Destination.find(JSON.parse(queryStr));
+  console.log(req.query, "KKK".blue);
   // execute query
-  const destinations = await query;
+  const features = new APIFeatures(Destination.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const destinations = await features.query;
+
+  // send response
   res.status(200).json({
     status: "success",
     results: destinations.length,
