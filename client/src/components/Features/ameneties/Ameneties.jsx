2f15f75510454e8d3ef85d7aa35dca93385c0f00
john-vasile <john.Vs11@hotmail.com>
2023-08-13 13:03:24 -0400

    fix unique key error

--- client/src/components/Features/ameneties/Ameneties.jsx
+++ client/src/components/Features/ameneties/Ameneties.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { ameneties, data } from '../../../data/data';
-import AmenetiesCard from './AmenetiesCard';
+import React from "react";
+import { ameneties } from "../../../data/data";
+import AmenetiesCard from "./AmenetiesCard";
 function Ameneties(props) {
   return (
-    <div className='max-w-[1600px] mx-auto  py-16 '>
-      <div className='p-4'>
+    <div className="max-w-[1600px] mx-auto  py-16 ">
+      <div className="p-4">
         <h1>Amazing ameneties</h1>
         <p>have a great time</p>
       </div>
-      <div className='grid grid-cols-2 gap-3 lg:grid-cols-3 xl:gap4'>
+      <div className="grid grid-cols-2 gap-3 lg:grid-cols-3 xl:gap4">
         {ameneties.map((item, index) => {
-          return <AmenetiesCard bg={item.image} text={item.name} />;
-        })}
-      </div>
-
-      <div className='grid grid-cols-2 gap-3 lg:grid-cols-3 xl:gap4'>
-        {data.map((item, index) => {
-          return <AmenetiesCard bg={item.image} text={item.name} key={index} />;
+          return (
+            <AmenetiesCard
+              bg={item.image}
+              text={item.name}
+              key={`ameneties${index}`}
+            />
+          );
         })}
       </div>
     </div>
