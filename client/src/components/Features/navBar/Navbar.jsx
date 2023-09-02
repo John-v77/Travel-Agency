f56d7fd1298f6ddc36cdd31c66d1c9535a494cf6
john-vasile <john.Vs11@hotmail.com>
2023-09-02 19:04:28 -0400

    fixed mobile menu not hiding after redirecting

--- client/src/components/Features/navBar/Navbar.jsx
+++ client/src/components/Features/navBar/Navbar.jsx
@@ -6,13 +6,7 @@ import { BiSearch } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
-import {
-  FaFacebook,
-  FaTwitter,
-  FaInstagram,
-  FaPinterest,
-  FaYoutube,
-} from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaInstagram, FaPinterest, FaYoutube } from "react-icons/fa";
 
 function Navbar(props) {
   const [mobNav, setMobNav] = useState(false);
@@ -45,38 +39,36 @@ function Navbar(props) {
       </div>
 
       <div onClick={handleNav} className="md:hidden  z-20 mobile-menu-btn ">
-        {mobNav ? (
-          <AiOutlineClose size={20} className="text-black" />
-        ) : (
-          <HiOutlineMenuAlt4 size={20} />
-        )}
+        {mobNav ? <AiOutlineClose size={20} className="text-black" /> : <HiOutlineMenuAlt4 size={20} />}
       </div>
 
-      <div
-        className={
-          mobNav
-            ? "md:hidden w-full h-[100vh] bg-gray-800/95 absolute left-0 top-0 z-10"
-            : "hidden"
-        }
-      >
+      <div className={mobNav ? "md:hidden w-full h-[100vh] bg-gray-800/95 absolute left-0 top-0 z-10" : "hidden"}>
         <div className="bg-gray-100/95 p-4 px-4  text-black">
           <h1 className="">Travel.</h1>
           <ul onClick={handleNav} className="flex flex-col md:hidden ">
             <li className="mobileLi">
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={handleNav}>
+                Home
+              </Link>
             </li>
             <li className="mobileLi">
-              <Link to="/destinations">Destinations</Link>
+              <Link to="/destinations" onClick={handleNav}>
+                Destinations
+              </Link>
             </li>
             <li className="mobileLi">
-              <Link to="/test">test</Link>
+              <Link to="/test" onClick={handleNav}>
+                test
+              </Link>
             </li>
           </ul>
 
           <div className="flex flex-col">
             <button className="my-6 border-none">Search</button>
             <button className="login border-none">
-              <Link to="/account">Account</Link>
+              <Link to="/account" onClick={handleNav}>
+                Account
+              </Link>
             </button>
           </div>
 
