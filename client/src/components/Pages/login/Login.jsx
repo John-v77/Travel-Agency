cf347a19a26670d97e39430a3fb6891167db8138
John-v77 <john.vs11@hotmail.com>
2023-09-10 17:04:10 -0400

    makes login page full height

--- client/src/components/Pages/login/Login.jsx
+++ client/src/components/Pages/login/Login.jsx
@@ -28,7 +28,7 @@ function Login(props) {
   };
 
   return (
-    <div className="">
+    <div className="min-h-screen">
       <img
         src="https://www.wallpaperup.com/uploads/wallpapers/2013/09/29/153361/44e5a3fd8a183ce3ab4d2130ba1b66bb.jpg"
         className="w-full h-20 object-cover object-top "
@@ -36,20 +36,35 @@ function Login(props) {
       />
 
       <div className="my-20">
-        <h2 className="text-2xl text-center my-4 md:my-6">Login</h2>
+        <h2 className="text-2xl text-center my-4 md:my-6">
+          Login
+        </h2>
         <div className="form  p-2 md:p3 rounded-md  max-w-xl mx-auto my-4">
           <form className="w-full border border-black rounded-md p-3 md:p-4">
             <div className="flex flex-col my-2">
               <label className="mx-1">Email</label>
-              <input onChange={recordInput} className="border rounded-md p-2" type="text" name="email" />
+              <input
+                onChange={recordInput}
+                className="border rounded-md p-2"
+                type="text"
+                name="email"
+              />
             </div>
             <div className="flex flex-col my-4">
               <label className="mx-1">Password</label>
 
-              <input className="border rounded-md p-2" onChange={recordInput} type="password" name="pswd" />
+              <input
+                className="border rounded-md p-2"
+                onChange={recordInput}
+                type="password"
+                name="pswd"
+              />
             </div>
 
-            <button onClick={submitForm} className="w-full my-4 border-none">
+            <button
+              onClick={submitForm}
+              className="w-full my-4 border-none"
+            >
               Login
             </button>
           </form>
