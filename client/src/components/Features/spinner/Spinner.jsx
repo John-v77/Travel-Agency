c47d79bb059728083912a6c6801ca42c23a86589
John-v77 <john.vs11@hotmail.com>
2023-09-10 17:55:16 -0400

    fix spinner error

--- client/src/components/Features/spinner/Spinner.jsx
+++ client/src/components/Features/spinner/Spinner.jsx
@@ -5,11 +5,9 @@ function Spinner(props) {
   return (
     <div className="my-20 min-h-screen">
       <div id="spinnerContainer">
-        <div id="img1" class="animationClass"></div>
-        <div id="img2" class="animationClass"></div>
-        <div id="text" class="">
-          Loading
-        </div>
+        <div id="img1" className="animationClass"></div>
+        <div id="img2" className="animationClass"></div>
+        <div id="text">Loading</div>
       </div>
     </div>
   );
