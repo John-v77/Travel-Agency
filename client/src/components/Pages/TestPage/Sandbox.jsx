f4b5dc59c893339f89ad6b011c5b391c6a336f85
john-vasile <john.Vs11@hotmail.com>
2023-09-28 09:44:15 -0400

    adds banner to sandbox page

--- client/src/components/Pages/TestPage/Sandbox.jsx
+++ client/src/components/Pages/TestPage/Sandbox.jsx
@@ -3,9 +3,14 @@ import React from "react";
 function Sandbox(props) {
   return (
     <div
-      className="mt-20 h-screen
+      className="h-screen
 "
     >
+      <img
+        src="https://www.wallpaperup.com/uploads/wallpapers/2013/09/29/153361/44e5a3fd8a183ce3ab4d2130ba1b66bb.jpg"
+        className="w-full h-20 object-cover object-top "
+        alt="navbar background"
+      />
       This is the test page
     </div>
   );
