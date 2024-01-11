f2d94868529825c2452fef7ea2ff611a7c3da5fc
john-vasile <john.Vs11@hotmail.com>
2024-01-11 12:04:16 -0500

    change Destinations Hero picture

--- client/src/components/Pages/Destinations/Destinations.jsx
+++ client/src/components/Pages/Destinations/Destinations.jsx
@@ -5,6 +5,7 @@ import { increment } from "../../../store/slices/counterSlice";
 import DestinationsDisplay from "../../Features/destinationsDisplay/DestinationsDisplay";
 import { GetProducts } from "../../../store/services/productsService";
 import Spinner from "../../Features/spinner/Spinner";
+import tropicalBg from "../../../assets/Tropical.jpg";
 function Destinations(props) {
   const { products } = useSelector((state) => state.products);
 
@@ -25,14 +26,18 @@ function Destinations(props) {
     return (
       <div className="max-w-[1600px] mx-auto md:p-0">
         <div className="max-h-[500px] relative">
-          <div className="absolute w-full h-full max-h-[500px] bg-black/40 flex flex-col justify-center text-gray-200 text-center">
-            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
-              The Best Travel Experience
-            </h1>
+          <div className="absolute w-full h-full max-h-[500px]  flex flex-col justify-center text-gray-200 text-center">
+            <div className="bg-black/80 py-5 px-10 m-auto inline-block w-fit">
+              <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl font-bold ">
+                The Best Travel Experience
+              </h1>
+            </div>
           </div>
           <img
-            className="w-full max-h-[500px] object-cover"
-             src="https://imageio.forbes.com/specials-images/dam/imageserve/1171238184/0x0.jpg?format=jpg&width=1600"
+            className="w-full max-h-[500px] object-cover object-top"
+            // src="https://imageio.forbes.com/specials-images/dam/imageserve/1171238184/0x0.jpg?format=jpg&width=1600"
+            // src="https://www.nerdwallet.com/assets/blog/wp-content/uploads/2021/03/GettyImages-479711885.jpg"
+            src={tropicalBg}
             alt="/"
           />
         </div>
